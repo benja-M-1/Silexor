af8af0af8e3dc34d3e542e8a790caf89d298ed73
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2011-10-23 23:43:27 +0200

    Add test for ProjectGenerator class.

--- src/Silexor/Tests/Generator/ProjectGeneratorTest.php
+++ src/Silexor/Tests/Generator/ProjectGeneratorTest.php
@@ -40,6 +40,11 @@ class ProjectGeneratorTest extends GeneratorTest
             $this->assertTrue(file_exists($this->tmpDir.'/test/'.$file), sprintf('"%s" has been generated in "%s"', $file, $this->tmpDir));
         }
 
-
+        $this->assertContains('$app = new Silex\Application();', file_get_contents($this->tmpDir.'/test/src/app.php'));
+        $this->assertContains('$app->run()', file_get_contents($this->tmpDir.'/test/web/index.php'));
+        $this->assertContains('class ControllerTest extends WebTestCase', file_get_contents($this->tmpDir.'/test/tests/ControllerTest.php'));
+        $this->assertContains('require_once __DIR__.\'/../vendor/silex.phar\';', file_get_contents($this->tmpDir.'/test/tests/bootstrap.php'));
+        $this->assertContains('bootstrap="tests/bootstrap.php"', file_get_contents($this->tmpDir.'/test/phpunit.xml.dist'));
+        $this->assertNotContains('{{ name }}', file_get_contents($this->tmpDir.'/test/phpunit.xml.dist'));
     }
 }
